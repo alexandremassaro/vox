ea847ed3515953d33230a2be56266db24cb617bb
Leonardo Fukuyama <leofukuyama22@gmail.com>
2024-05-25 10:45:49 -0300

    Adicionado atributos para as Entidades, Constructor e @Override em toString().

--- src/model/LinhaProducao.java
+++ src/model/LinhaProducao.java
@@ -1,8 +1,18 @@
 package model;
 
 public class LinhaProducao {
-    private int linhaId;
-    private String nome;
+    private final int linhaId;
+    private final String nome;
+
+    public LinhaProducao(int linhaId, String nome) {
+        this.linhaId = linhaId;
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + this.linhaId + ", Nome: " + this.nome;
+    }
 
     // Getters e Setters
 }
